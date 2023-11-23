467e7e9f2ac76ef1a1ffffb671e105cf103d9b2d
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-11-23 14:16:49 +0530

    added uuid package for unique identification of users

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,22 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
-const users = [
-    {
-        firstName:"John",
-        lastName: "Doe",
-        age: 25
-    }
-]
+const users = [];
 //all routers start from '/user' hence only / is required
 router.get('/', (req, res)=>{
     console.log(users);
     res.send(users);
 });
 
+//all routes starting from here are from "/users" specifyed in index.js
 router.post('/', (req, res) =>{
-     console.log("POST Route REACHED");
-     res.send("POST Route REACHED")
+    const user = req.body
+
+    //... => spread operator it spreads the array 
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with name ${user.firstName} added to the DataBase!`)
 } );
 
 export default router;
